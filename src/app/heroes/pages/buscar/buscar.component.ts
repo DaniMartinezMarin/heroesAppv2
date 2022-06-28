715bd18a83599b93d94bd72c6089aa1630398877
Daniel Martinez <dmartmar@mercadona.es>
2022-06-28 12:15:50 +0200

    feat: Buscador de heroes

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Hero[] = [];
+  heroeSeleccionado: Hero | undefined;
+
+  constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
+  }
+
+  getSugerencias(): void {
+
+    if(!this.termino.trim()){
+      this.heroes = [];
+      return;
+    }
+
+    this.heroesService.getHeroesSugerencias( this.termino.trim() )
+      .subscribe( ( heroes: Hero[] ) => this.heroes = heroes )
+  }
+
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ): void {
+
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
+    const heroe: Hero = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroeById( heroe.id! )
+      .subscribe( (heroe: Hero) => this.heroeSeleccionado = heroe );
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Hero } from '../interfaces/hero.interface';
 import { Observable } from 'rxjs';
+
+import { Hero } from '../interfaces/hero.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  urlHeroes: string = 'http://localhost:3000/heroes';
+  private apiUrl: string = environment.apiEndpoint;
 
   constructor( private ngHttpClient: HttpClient ) { }
 
   getHeroesFromDb(): Observable<Hero[]> {
-    return this.ngHttpClient.get<Hero[]>(this.urlHeroes);
+    return this.ngHttpClient.get<Hero[]>(`${ this.apiUrl }/heroes`);
   }
 
   getHeroeById ( id: string ): Observable<Hero> {
-    return this.ngHttpClient.get<Hero>(`${this.urlHeroes}/${id}`);
+    return this.ngHttpClient.get<Hero>(`${ this.apiUrl }/heroes/${ id }`);
   }
+
+  getHeroesSugerencias ( termino: string ): Observable<Hero[]> {
+    return this.ngHttpClient.get<Hero[]>(`${ this.apiUrl }/heroes?q=${ termino }&_limit=6`);
+  }
+
 }
