cd131f21bd5fa26ccb0da565245c0fb2ad3e218e
Daniel Martinez <dmartmar@mercadona.es>
2022-07-13 17:33:02 +0200

    feat: Anyadida informacion de id al local storage y verificamos esos datos en json server

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthInterface } from '../interfaces/auth-interface';
 
@@ -16,18 +16,33 @@ export class AuthService {
     return {...this._auth!};
   }
 
+  verificaAutenticacion(): Observable<boolean> {
+
+    if( !localStorage.getItem('authToken') ) {
+      return of(false);
+    }
+
+    return this.htpp.get<AuthInterface>(`${this.baseUrl}/usuarios/1`)
+      .pipe(
+        map( auth => {
+          this._auth = auth;
+          return true;
+        } )
+      )
+  }
+
   constructor( private htpp: HttpClient ) { }
 
   login(): Observable<AuthInterface>{
     return this.htpp.get<AuthInterface>(`${this.baseUrl}/usuarios/1`)
       .pipe(
         tap( (auth: AuthInterface) => this._auth = auth ),
-        tap( (auth: AuthInterface) => localStorage.setItem('id', auth.id) )
+        tap( (auth: AuthInterface) => localStorage.setItem('authToken', auth.id) )
       );
   }
 
   logout(): void {
     this._auth = undefined;
-    localStorage.removeItem('id');
+    localStorage.removeItem('authToken');
   }
 }
